ecf9b681b15cdcc54bd31a47e4988922c07a1d3a
SithPrincess <Cassie2727@gmail.com>
2024-12-27 22:22:27 -0600

    Added Js-Base

---
+++ index.js
@@ -0,0 +1,51 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityBox = document.querySelector("#current-city");
+  searchCity(searchInputElement.value);
+}
+
+function searchCity(city) {}
+
+function getWeather(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector(".current-temperature-value");
+  temperatureElement.innerHTML = temperature;
+  let cityBox = document.querySelector("#current-city");
+  cityBox.innerHTML = response.data.city;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
